747c62c0b44cca6623470ea276810eeaeae28159
Jakub Rynio <jakubrynio12@protonmail.com>
2023-11-25 09:41:27 +0100

    Podatki koniec pracy

--- podatki/main.cpp
+++ podatki/main.cpp
@@ -1,3 +1,8 @@
+/*
+Autor: Jakub Rynio
+Opis: Program obliczajacy podatek w roku 2003
+W razie pytan lub uwag pisz do mnie na mesanger lub mail - RynioJakub@proton.me
+*/
 # include <iostream>
 # include <cmath>
 
@@ -18,15 +23,15 @@ int main(){
     if(zarobki < 2789.89){
         cout<<"Nie zaplacisz od tego podatku :)";
 
-    } else if(zarobki < 37024 && zarobki > 2789.89){
-        cout<<"Zaplacisz: "<<ceil((zarobki * 0.19 - 530.08) * 100)/100 ;
+    } else if(zarobki > 2789.89 && zarobki < 37024){
+        cout<<"Zaplacisz: "<<ceil((zarobki * 0.19 - 530.08) * 100)/100<<" PLN podatku";
 
-    } else if(zarobki < 74048 && zarobki > 37024){
-        cout<<"Zaplacisz: "<< ceil(6504.48 + ((zarobki - 37024) * 0.3)*100)/100;
+    } else if(zarobki > 37024 && zarobki < 74048){
+        cout<<"Zaplacisz: "<< ceil(6504.48 + ((zarobki - 37024) * 0.3)*100)/100<<" PLN podatku";
 
     } else if(zarobki > 74048){
-        cout<<"Zaplacisz: "<< ceil(17611.68 + ((zarobki-74048) * 0.4)*100)/100;
+        cout<<"Zaplacisz: "<< ceil(17611.68 + ((zarobki-74048) * 0.4)*100)/100<<" PLN podatku";
     }
-// PROGRAM NIEDOKONCZONY
+
     return 0;
 }
